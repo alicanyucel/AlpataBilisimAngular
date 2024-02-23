e036678fd9848368e9ce9da77b527bba1cb88383
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-02-24 00:38:03 +0300

    componetler eklendi...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { MeetComponent } from './meet/meet.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { SendMailComponent } from './send-mail/send-mail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MeetComponent,
+    FileUploadComponent,
+    SendMailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meet/meet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meet',
+  templateUrl: './meet.component.html',
+  styleUrls: ['./meet.component.css']
+})
+export class MeetComponent {
+
+}

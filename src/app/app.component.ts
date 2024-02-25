34af8e1b06c0c139f5340b328e1c8b293d3ff7de
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-02-25 16:03:50 +0300

    ikon düzeltildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ import { ToastrService } from 'ngx-toastr';
         </li>
         <li class="nav-item">
         <a class="nav-link active" aria-current="page" routerLink="/file">
-        <i class="fa fa-home">Dosya Yükle</i> 
+        <i class="fa-solid fa-file">Dosya Yükle</i> 
        </a>
       </li>
       </ul>

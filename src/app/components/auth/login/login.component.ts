60af87fc4d9ecf8dcc012426bcebf3fec5a514b4
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-02-25 15:37:19 +0300

    lopgin sayfası yapıldı

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
-}
+export class LoginComponent implements OnInit {
+  constructor(
+    private _auth:AuthService
+  ){}
+  ngOnInit():void{}
+  login(loginForm: NgForm){
+  if(loginForm.valid)
+  {
+   this._auth.login(loginForm.value);
+  }
+  }
+  }

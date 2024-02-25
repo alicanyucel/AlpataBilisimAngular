7559a2052e09cc63a3819a736c3ce5a3cba07869
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-02-25 11:57:39 +0300

    auth service added.

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserModel } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+api:string="https://localhost:7245/api"+"/Auth";
+  constructor(private _http:HttpClient,
+    private _router:Router) { }
+    login(model:UserModel)
+    {
+      this._http.post<UserModel>(this.api+"/Login",model).subscribe({
+        next:(res)=>{
+          localStorage.setItem("user",JSON.stringify(res));
+          this._router.navigateByUrl("/");
+        }
+      })
+    }
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class UserModel{
+    Name:string;
+    LastName:string;
+    Email:string;
+    UserName:string;
+    Password:string;
+    PhoneNumber:string;
+    ImageUrl:string;
+}
